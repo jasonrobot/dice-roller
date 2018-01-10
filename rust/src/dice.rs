ac4f58aad067fbde75aae1ae4942669bb03c7fe3
Jason Howell <jasonrobot64@gmail.com>
2018-01-09 19:39:03 -0800

    just gonna keep the test module in the module it tests. Kinda like it

--- rust/src/dice.rs
+++ rust/src/dice.rs
@@ -12,3 +12,14 @@ pub fn roll_dice(amount: u32, sides: u32) -> Vec<u32> {
     }
     v
 }
+
+
+#[cfg(test)]
+mod tests {
+    use dice;
+    #[test]
+    fn samle_test() {
+        assert_eq!(2 + 2, 4);
+        assert!( dice::roll_dice(3, 4).len() == 3 );
+    }
+}
--- rust/src/test.rs
+++
@@ -1,9 +0,0 @@
-
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn lets_see() {
-        assert_eq!(2 + 2, 4);
-    }
-}
