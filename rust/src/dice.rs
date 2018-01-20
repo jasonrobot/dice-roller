bb37b299245b39680fd7703c05d176a49a52dd9c
Jason Howell <jasonrobot64@gmail.com>
2018-01-20 11:13:37 -0800

    finally figuring Rust out :)

--- rust/src/dice.rs
+++ rust/src/dice.rs
@@ -2,9 +2,30 @@ extern crate rand;
 
 use self::rand::Rng;
 // use self::rand::distributions::{Range, IndependentSample};
+#[derive(Debug)]
+pub struct Dice {
+    amount: i32,
+    sides: i32,
+}
 
-pub fn roll_dice(amount: u32, sides: u32) -> Vec<u32> {
-    let mut v: Vec<u32> = Vec::new();
+impl Dice {
+    #![allow(dead_code)]
+    pub fn new(amount: i32, sides: i32) -> Dice {
+        Dice {amount, sides}
+    }
+
+    pub fn from_string(line: &str) -> Dice {
+        let (amount, sides) = parse(line);
+        Dice {amount, sides}
+    }
+
+    pub fn roll(&self) -> Vec<i32> {
+        _roll(self.amount, self.sides)
+    }
+}
+
+fn _roll(amount: i32, sides: i32) -> Vec<i32> {
+    let mut v: Vec<i32> = Vec::new();
     let mut rng = rand::thread_rng();
 
     for _ in 0..amount {
@@ -13,13 +34,46 @@ pub fn roll_dice(amount: u32, sides: u32) -> Vec<u32> {
     v
 }
 
+fn parse(line: &str) -> (i32, i32) {
+    if !is_valid(line) {
+        panic!("invalid line");
+    }
+    let split = line.split("d");
+    let vec: Vec<&str> = split.collect();
+    (vec[0].parse::<i32>().unwrap(), vec[1].parse::<i32>().unwrap())
+}
+
+fn is_valid(line: &str) -> bool {
+    line.matches(r"^(\d*)d(\d+)$").count() == 1
+}
+
 
 #[cfg(test)]
 mod tests {
-    use dice;
     #[test]
-    fn samle_test() {
+    fn sample_test() {
         assert_eq!(2 + 2, 4);
-        assert!( dice::roll_dice(3, 4).len() == 3 );
+        assert!( super::_roll(3, 4).len() == 3 );
+        let d = super::parse("2d12");
+        println!("{:?}", super::_roll(d.0, d.1));
+    }
+
+    #[test]
+    fn test_parse() {
+        let result = super::parse("3d6");
+        assert_eq!(result, (3, 6));
+    }
+
+    #[test]
+    fn test_use_dice() {
+        let d = super::Dice::new(6, 6);
+        d.roll();
+    }
+
+    #[test]
+    fn test_parse_does_not_allow_negative_values() {
+        let result = super::parse("-3d4");
+        println!("{:?}", result);
+        assert!(result.0 != -3);
     }
 }
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -3,8 +3,11 @@ mod dice;
 fn main() {
     // println!("basic method: {}", rand::random::<u64>());
 
-    println!("using a range,");
-    println!("d6: {:?}", dice::roll_dice(1, 6));
-    println!("2d12: {:?}", dice::roll_dice(2, 12));
-    println!("4d4: {:?}", dice::roll_dice(4, 4));    
+    println!("using the real thing,");
+    let d = dice::Dice::from_string("20d6");
+    println!("d6: {:?}", d.roll());
+    let d = dice::Dice::from_string("20d12");
+    println!("2d12: {:?}", d.roll());
+    let d = dice::Dice::from_string("20d100");
+    println!("4d4: {:?}", d.roll());
 }
