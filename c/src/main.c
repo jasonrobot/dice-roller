0f85e55a82228717de509c9574457ffcafb4d0ab
Jason Howell <jasonrobot64@gmail.com>
2018-01-25 00:01:48 -0800

    now is not the time for pointers. lets see if I can figure this out tomorrow...

--- c/src/main.c
+++ c/src/main.c
@@ -2,6 +2,7 @@
 #include "stdlib.h"
 #include "time.h"
 #include "dice.h"
+#include "string.h"
 
 int main(void) {
     printf("4\nbut really...\n");
@@ -21,5 +22,29 @@ int main(void) {
     }
     printf("]\n");
     free(results);
+
+    int running = 1;
+    char input[BUFSIZ];
+    char firstnum[BUFSIZ];
+    char secondnum[BUFSIZ];
+    while (running) {
+        if ( fgets(input, BUFSIZ, stdin) == NULL ) {
+            continue;
+        }
+        printf("%s\n", input);
+        if (input[0] == 'q') {
+            running = 0;
+        }
+        char* d_char = NULL;
+        d_char = strchr(input, 'd');
+        if (d_char == NULL) {
+            printf("Unable to parse input: %s\n", input);
+            continue;
+        }
+        strncpy(firstnum, input, (d_char-firstnum)/sizeof(char));
+        strcpy(secondnum, d_char + sizeof(char));
+        printf("%s, %s", firstnum, secondnum);
+    }
+
     return 0;
 }
