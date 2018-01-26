f8819e6dbe32c906208140ef063d4734f4012a98
Jason Howell <jhowell@ontraport.com>
2018-01-25 17:02:45 -0800

    clojure version works, but needs tests and error handling

--- clojure/dice-roller/src/dice_roller/core.clj
+++ clojure/dice-roller/src/dice_roller/core.clj
@@ -2,22 +2,36 @@
   (:gen-class)
   (:require [clojure.string :as str]))
 
+(defn add-one
+  [num]
+  (+ 1 num))
+
 (defn roll
   "Roll the dice"
   ([amount sides]
-    (map rand-int (repeat amount sides)))
+    (map add-one (map rand-int (repeat amount sides))))
   ([sides]
-    (list (rand-int sides))))
+    (list (add-one (rand-int sides)))))
 
 (defn parse
   "parse dice to roll from a string"
   [line]
   (map read-string (str/split line #"d")))
 
+(defn main-loop
+  "loop on user input"
+  []
+  (print "=> ")
+  (flush)
+  (let [line (read-line)]
+    (cond
+      (= line "q") nil
+      :else (do 
+              (println (apply roll (parse line)))
+              (recur)))))
+
 (defn -main
   "just some examples for now"
   [& args]
-  (println "Hello, World!")
-  (println (roll 6 6))
-  (println (apply roll (parse "4d6")))
-  (println (roll 20)))
+  (println "Welcome!")
+  (main-loop))
