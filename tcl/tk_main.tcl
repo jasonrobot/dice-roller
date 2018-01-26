d65860071f06cb80b84e9399f420ae41aa64b713
Jason Howell <jhowell@ontraport.com>
2018-01-26 09:36:16 -0800

    fixed up the GUI in tcl/tk

--- tcl/tk_main.tcl
+++ tcl/tk_main.tcl
@@ -4,20 +4,33 @@
 
 source src/dice.tcl
 
-label .dice_label -text "Enter dice:"
-entry .dice_input -bg white
-button .roll_button -text "roll!" -command {
-    set dice [parse [.dice_input get]]
+frame .c -padx 5 -pady 5
+
+proc doRoll {} {
+    set dice [parse [.c.diceInput get]]
     if {[lindex $dice 0] == 0} {
         set result "Invalid format"
     } else {
         set result [roll {*}$dice]
     }
-    .result_label configure -text $result
+    .c.resultValueLabel configure -text $result
 }
-label .result_label
 
-#pack .dice_label .dice_input .result_label .roll_button
-grid .dice_label .dice_input .roll_button .result_label
-grid columnconfigure . {0 1 2} -weight 1
-# grid rowconfigure . {0 1}
+label .c.diceLabel -text "Enter dice:"
+entry .c.diceInput -bg white
+bind .c.diceInput <Return> doRoll
+
+button .c.rollButton -text "roll!" -command doRoll
+
+label .c.resultLabel -text "Result:"
+label .c.resultValueLabel
+
+grid .c -column 0 -row 0 -sticky nwes
+
+grid .c.diceLabel -column 0 -row 0 -sticky nwes
+grid .c.diceInput -column 1 -row 0 -sticky nwes
+grid .c.rollButton -column 1 -row 1 -sticky nwes
+grid .c.resultLabel -column 0 -row 2 -sticky news
+grid .c.resultValueLabel -column 1 -row 2 -sticky nwes
+
+foreach w [winfo children .c] {grid configure $w -padx 10 -pady 10}
